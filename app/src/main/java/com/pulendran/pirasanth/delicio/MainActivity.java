4ad655b236d2883602e30b69d37a1c44b25a24b5
60950321 <P60950321@hotmail.com>
2015-03-03 10:38:25 +0100

    slet af import import android.view.Menu; import android.view.MenuItem;
    + to metoder til actionbar
    
    lavet relative layout clickable og log tekst til

--- app/src/main/java/com/pulendran/pirasanth/delicio/MainActivity.java
+++ app/src/main/java/com/pulendran/pirasanth/delicio/MainActivity.java
@@ -2,19 +2,22 @@ package com.pulendran.pirasanth.delicio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("pelle","test");
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.d("pelle","klikk");
 
-
-
-
+    }
 }
