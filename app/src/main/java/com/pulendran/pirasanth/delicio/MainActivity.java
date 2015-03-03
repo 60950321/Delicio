db8e3a692ef7e22c67ad1564a54edecc72f3fa32
60950321 <P60950321@hotmail.com>
2015-03-03 10:48:56 +0100

    android lifecycle metoder tilføjet mainact

--- app/src/main/java/com/pulendran/pirasanth/delicio/MainActivity.java
+++ app/src/main/java/com/pulendran/pirasanth/delicio/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
+import android.support.annotation.NonNull;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
@@ -13,6 +13,60 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("pelle","test");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("pelle", "onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("pelle", "onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pelle", "onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("pelle", "onStop");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("pelle", "onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("pelle", "onDestroy");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("pelle", "onSaveInstanceState");
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i("pelle", "onRestoreInstanceState");
     }
 
     @Override
